a58a3094d27976f82070de7082c0e3617a3379aa
Jay Yadav <jskk15536@gmail.com>
2026-01-21 20:16:32 +0530

    remove the app scert and key

--- src/main/java/com/integratez/platform/modules/shopify/config/WebClientConfig.java
+++ src/main/java/com/integratez/platform/modules/shopify/config/WebClientConfig.java
@@ -12,7 +12,7 @@ public class WebClientConfig {
         return WebClient.builder()
                 .baseUrl("https://" + "wffbqx-si" + ".myshopify.com/admin/api/2024-10/graphql.json")
                 .defaultHeader("Content-Type", "application/json")
-                .defaultHeader("X-Shopify-Access-Token", "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")
+                .defaultHeader("")
                 .build();
     }
 
